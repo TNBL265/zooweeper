f504ab5923d4b94f516175e447b929fd94c359a7
TNBL265 <long_tran@mymail.sutd.edu.sg>
2023-10-31 14:10:31 +0800

    :sparkles: create ZooKeeper ensemble

--- server/main.go
+++ server/main.go
@@ -4,58 +4,59 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	ZNodeHandlers "github.com/tnbl265/zooweeper/database/handlers"
+	ensemble "github.com/tnbl265/zooweeper/ensemble"
 	"log"
 	"net/http"
 	"os"
-
-	ZNodeHandlers "github.com/tnbl265/zooweeper/database/handlers"
-	requestProcessor "github.com/tnbl265/zooweeper/request_processors"
+	"strconv"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = "8080"
 	}
+	port, _ := strconv.Atoi(portStr)
+
+	leader := 8080
+	allServers := []int{8080, 8081, 8082}
 
 	var dbPath string
 
 	switch port {
-	case "8080":
+	case 8080:
 		dbPath = "database/zooweeper-metadata-0.db"
-	case "8081":
+	case 8081:
 		dbPath = "database/zooweeper-metadata-1.db"
-	case "8082":
+	case 8082:
 		dbPath = "database/zooweeper-metadata-2.db"
 	default:
-		log.Fatalf("Unsupported port: %s", port)
+		log.Fatalf("Only support ports 8080, 8081 or 8082")
 	}
 
-	// Initialization code here
-	fmt.Println("ZooWeeper Server started on port:", port)
-
-	// Set Application Config
-	var rp requestProcessor.RequestProcessor
+	// Initialization Server
+	log.Println("ZooWeeper Server started on port:", port)
+	server := ensemble.NewServer(port, leader, allServers)
 
 	// Connect to the Database
 	log.Println("Connecting to", dbPath)
-	db, err := rp.Zab.OpenDB(dbPath)
+	db, err := server.Rp.Zab.OpenDB(dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rp.Zab.ZTree = &ZNodeHandlers.ZTree{DB: db}
-	//close when it is done
+	server.Rp.Zab.ZTree = &ZNodeHandlers.ZTree{DB: db}
 	defer func(connection *sql.DB) {
 		err := connection.Close()
 		if err != nil {
 			log.Fatal(err)
 		}
-	}(rp.Zab.ZTree.Connection())
+	}(server.Rp.Zab.ZTree.Connection())
 
-	// Start a Web Server
-	log.Println("Starting rplication on port", port)
-	err = http.ListenAndServe(fmt.Sprintf(":"+port), rp.Routes())
+	// Start Server
+	log.Printf("Starting Server (%s) on port %s\n", server.State(), portStr)
+	err = http.ListenAndServe(fmt.Sprintf(":"+portStr), server.Rp.Routes())
 	if err != nil {
 		log.Fatal(err)
 	}
