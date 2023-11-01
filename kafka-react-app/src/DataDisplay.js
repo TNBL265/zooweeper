e70dffb739374b8168b0f0e54a25cff65793651e
TNBL265 <long_tran@mymail.sutd.edu.sg>
2023-11-02 03:00:08 +0800

    :sparkles: pick random Kafka Server

--- kafka-react-app/src/DataDisplay.js
+++ kafka-react-app/src/DataDisplay.js
@@ -1,35 +1,42 @@
-// src/DataDisplay.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function DataDisplay() {
-  const [data, setData] = useState([]);
-  const port = 9090;
-  console.log(data, "data");
-  useEffect(() => {
-    // HTTP GET request to your Kafka server's /data endpoint
-    axios
-      .get(`http://localhost:${port}/data`)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+    const [data, setData] = useState([]);
 
-  return (
-    <div>
-      <h1>Data Display from Kafka Server</h1>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>
-            {item.Min} mins - {item.Player} ({item.Club}) - Score: {item.Score}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    // Helper function to get a random port
+    const getRandomPort = () => {
+        const ports = [9090, 9091, 9092];
+        return ports[Math.floor(Math.random() * ports.length)];
+    };
+
+    const port = getRandomPort();
+    console.log("Kafka Server: ", port);
+
+    useEffect(() => {
+        // HTTP GET request to your Kafka server's /data endpoint
+        axios
+            .get(`http://localhost:${port}/data`)
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Data Display from Kafka Server</h1>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>
+                        {item.Min} mins - {item.Player} ({item.Club}) - Score: {item.Score}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default DataDisplay;
