a5a4015d78d76f19f6d4885c22f75f2a6b92a545
demeritbird <lekjiewei@hotmail.sg>
2023-11-23 05:40:50 +0800

    feat: wakeup start election

--- server/main.go
+++ server/main.go
@@ -37,13 +37,13 @@ func main() {
 	switch port {
 	case 8080:
 		dbPath = "database/zooweeper-metadata-0.db"
-		state = ensemble.LEADING
+		state = ensemble.FOLLOWING
 	case 8081:
 		dbPath = "database/zooweeper-metadata-1.db"
 		state = ensemble.FOLLOWING
 	case 8082:
 		dbPath = "database/zooweeper-metadata-2.db"
-		state = ensemble.FOLLOWING
+		state = ensemble.LEADING
 	default:
 		log.Fatalf("Only support ports 8080, 8081 or 8082")
 	}
@@ -61,6 +61,7 @@ func main() {
 	// Listen for errors
 	var err error
 	myAB := &AtomicBroadcastCopy{AtomicBroadcast: server.Rp.Zab}
+	go myAB.wakeupLeaderElection(server, port, leader, allServers)
 	go myAB.listenForLeaderElection(server, port, leader, allServers)
 	go func() {
 		_, err := ping(server, portStr)
@@ -70,6 +71,7 @@ func main() {
 			fmt.Println("Ping successful")
 		}
 	}()
+
 	// I
 	initZNode(server, port, leader, allServers)
 
@@ -105,7 +107,7 @@ func initZNode(server *ensemble.Server, port, leader int, allServers []int) {
 // If the ping response takes more than an arbitruary time, or the connection is refused from the other server,
 // then call the Error Channel with error information.
 func ping(server *ensemble.Server, currentPort string) (string, error) {
-	const PING_TIMEOUT = 10   // Arbitruary wait timer to simulate response time arrival
+	const PING_TIMEOUT = 5    // Arbitruary wait timer to simulate response time arrival
 	const REQUEST_TIMEOUT = 2 // Arbitruary wait timer to simulate response time arrival
 	for {
 
@@ -157,6 +159,7 @@ func ping(server *ensemble.Server, currentPort string) (string, error) {
 				errorData := models.HealthCheckError{
 					Error:     err,
 					ErrorPort: v,
+					IsWakeup:  false,
 				}
 				server.Rp.Zab.ErrorLeaderChan <- errorData
 				continue
@@ -196,13 +199,33 @@ type AtomicBroadcastCopy struct {
 	zooweeper.AtomicBroadcast // Embedding the type from the external package
 }
 
+func (ab *AtomicBroadcastCopy) wakeupLeaderElection(server *ensemble.Server, port int, leader int, allServers []int) {
+	for {
+		select {
+		case <-time.After(time.Second * 2):
+			// On wake-up, start leader-election
+			data := models.HealthCheckError{
+				Error:     nil,
+				ErrorPort: fmt.Sprintf("%d", port),
+				IsWakeup:  true,
+			}
+			ab.ErrorLeaderChan <- data
+			return
+		}
+	}
+}
 func (ab *AtomicBroadcastCopy) listenForLeaderElection(server *ensemble.Server, port int, leader int, allServers []int) {
 	for {
 		select {
 		case errorData := <-ab.ErrorLeaderChan:
 			errorPortNumber, _ := strconv.Atoi(errorData.ErrorPort)
-			if errorPortNumber == leader {
-				color.Magenta("Error from ping healthcheck! Starting leader election here...")
+			if errorPortNumber == leader || errorData.IsWakeup {
+				if errorPortNumber == leader {
+					color.Magenta("Error from port %d healthcheck! Starting leader election here...", errorPortNumber)
+				} else if errorData.IsWakeup {
+					color.Magenta("Port %d just woke up! Starting leader election here...", port)
+				}
+
 				ab.startLeaderElection(server, port, allServers)
 			}
 		}
