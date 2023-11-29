8c6654319f15621db6c19b751633f1f8655491de
TNBL265 <long_tran@mymail.sutd.edu.sg>
2023-11-29 15:30:33 +0800

    :recycle: remove unused fields from Server ensemble

--- server/ensemble/server.go
+++ server/ensemble/server.go
@@ -4,60 +4,12 @@ import (
 	rp "github.com/tnbl265/zooweeper/request_processors"
 )
 
-type ServerState string
-
-const (
-	LEADING   ServerState = "LEADING"
-	FOLLOWING ServerState = "FOLLOWING"
-	//ELECTING
-)
-
 type Server struct {
-	nodeId     int
-	leader     int
-	allServers []int
-	state      ServerState
-	Rp         rp.RequestProcessor
+	Rp rp.RequestProcessor
 }
 
-func NewServer(port, leader int, state ServerState, allServers []int, dbPath string) *Server {
+func NewServer(dbPath string) *Server {
 	return &Server{
-		nodeId:     port,
-		leader:     leader,
-		state:      state,
-		allServers: allServers,
-		Rp:         *rp.NewRequestProcessor(dbPath),
+		Rp: *rp.NewRequestProcessor(dbPath),
 	}
-}
-
-func (svr *Server) NodeId() int {
-	return svr.nodeId
-}
-
-func (svr *Server) SetNodeId(nodeId int) {
-	svr.nodeId = nodeId
-}
-
-func (svr *Server) Leader() int {
-	return svr.leader
-}
-
-func (svr *Server) SetLeader(leader int) {
-	svr.leader = leader
-}
-
-func (svr *Server) AllServers() []int {
-	return svr.allServers
-}
-
-func (svr *Server) SetAllServers(allServers []int) {
-	svr.allServers = allServers
-}
-
-func (svr *Server) State() ServerState {
-	return svr.state
-}
-
-func (svr *Server) SetState(state ServerState) {
-	svr.state = state
 }
